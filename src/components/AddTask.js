741fd200c8a050bfbaf84aa6b9fc645068f204bd
pawel2557 <pawelnowak984@gmail.com>
2025-04-18 11:02:00 +0200

    added tasks to local storage

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -2,7 +2,7 @@ const AddTask = ({ tasklist, setTasklist, task, setTask }) => {
   const handleSubmit = (event) => {
     event.preventDefault();
     const date = new Date();
-    console.log(task.id);
+    console.log(task);
     if (task.id) {
       const updatedTaskList = tasklist.map((taskIn)=> (taskIn.id===task.id) ? 
       {
@@ -26,7 +26,7 @@ const AddTask = ({ tasklist, setTasklist, task, setTask }) => {
     <section className="addTask">
       <form onSubmit={handleSubmit}>
         <input type="text" name="task" value={task.name || ""} autoComplete="off" placeholder="add a task" maxLength={25} onChange={e => setTask({...task, name: e.target.value})} />
-        <button type="submit">Add</button>
+        <button type="submit">{task.id ? "Update" : "Add"}</button>
       </form>
     </section>
   )
